b4f4db01ff684e4d5a26bca19f918ec2d489ae41
FazKal <fazelkalhory@gmail.com>
2024-08-16 13:04:58 +0330

    added static files and urls in setting

--- mySite/view.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def http_test(request):
-    return HttpResponse ('Hello')
--- website/views.py
+++ website/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index_view(requests):
-    return HttpResponse ('<h1>Home page</h1>')
+    return render (requests,'website/index.html')
 
 def about_view(requests):
-    return HttpResponse ('<h1>About page</h1>')
+    return render (requests,'website/about.html')
 
 def contact_view(requests):
-    return HttpResponse ('<h1>Contact page</h1>')
+    return render (requests,'website/contact.html')
 
 
