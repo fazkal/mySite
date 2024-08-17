b4865f9295a7cf9cff23bcd7c7aa6d6d552d4dd8
FazKal <fazelkalhory@gmail.com>
2024-08-17 12:04:26 +0330

    Define static root and media in settings.py

--- mySite/settings.py
+++ mySite/settings.py
@@ -126,4 +126,9 @@ STATIC_ROOT=BASE_DIR / 'static'
 MEDIA_URL= '/media/'
 MEDIA_ROOT=BASE_DIR / 'media'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "statics",
+
+]
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
