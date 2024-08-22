1b051745c88f6b724278a7faf07a13ff26b73225
FazKal <fazelkalhory@gmail.com>
2024-08-23 00:27:53 +0330

    Added some features to admin for post1 of blog such as 'date_hierarchy' till 'search field'

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from blog.models import Post1
 
 # Register your models here.
-
-admin.site.register(Post1)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy='created_date'
+    empty_value_display='-empty-'
+    list_display=('title','content_view','status','published_date','created_date')
+    list_filter=('status',)
+    ordering=['created_date']
+    search_fields=['title','content']
+admin.site.register(Post1,PostAdmin)
