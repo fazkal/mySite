c6a78650b476c3ae0fdff6cf1350ddc0dd5fe65e
FazKal <fazelkalhory@gmail.com>
2024-08-22 19:44:00 +0330

    make model (table) named 'post1' for blog and 'contact' for website apps and migrate them

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class Post (models.Model):
+class Post1 (models.Model):
     title= models.CharField(max_length=30)
     content=models.TextField()
+    content_view=models.IntegerField(default=0)
+    status=models.BooleanField(default=False)
+    published_date=models.DateTimeField(null=True)
+    created_date=models.DateTimeField(auto_now_add=True)
+    updated_date=models.DateTimeField(auto_now=True)
