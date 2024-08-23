50b7c0d63b6c0767a0ff784d5fa2f89427e47c79
FazKal <fazelkalhory@gmail.com>
2024-08-23 12:38:16 +0330

    Fetch data from tables (post table) and show in website by define condition in blog/view.py and tag that in templates/blog/blog-home.html between {for loop} for posts

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from blog.models import Post1
 
 def blog_view(requests):
-    return render(requests,'blog/blog-home.html')
+    posts=Post1.objects.filter(status=1)
+    context={'posts' : posts}
+    return render(requests,'blog/blog-home.html',context)
 
 def blog_single(requests):
     return render(requests,'blog/blog-single.html')
