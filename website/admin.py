ce59660a04bcfdba8dc3224d2ca6bf58503d52c8
FazKal <fazelkalhory@gmail.com>
2024-08-23 10:50:35 +0330

    set admin panel for website/contact by add class in it's admin.py

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from website.models import contact
 
 # Register your models here.
+class websiteAdmin(admin.ModelAdmin):
+    date_hierarchy='created_date'
+    list_display=('name','subject','created_date')
+    list_filter=('email',)
+    ordering=['created_date']
+    search_fields=('name','subject','message')
+admin.site.register(contact ,websiteAdmin)
