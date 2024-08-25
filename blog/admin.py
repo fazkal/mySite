38e3364a9c25cda341d44586986a583f3a02869a
FazKal <fazelkalhory@gmail.com>
2024-08-25 13:28:09 +0330

    Make a model named 'categorys' and registration in admin, create a field named 'image' and media folder for maintenance uploaded files, define a field named 'category', define tags in blog template

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-from blog.models import Post1
+from blog.models import Post1,categorys
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy='created_date'
     empty_value_display='-empty-'
-    list_display=('title','content_view','status','published_date','created_date')
-    list_filter=('status',)
+    list_display=('title','author','content_view','status','published_date','created_date')
+    list_filter=('status','author')
     ordering=['created_date']
     search_fields=['title','content']
+
 admin.site.register(Post1,PostAdmin)
+admin.site.register(categorys)
--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+class categorys(models.Model):
+    name=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 class Post1 (models.Model):
+    image=models.ImageField(upload_to='blog/',default='blog/Default.jpg')
+    author=models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     title= models.CharField(max_length=30)
     content=models.TextField()
+    category=models.ManyToManyField(categorys)
     content_view=models.IntegerField(default=0)
     status=models.BooleanField(default=False)
     published_date=models.DateTimeField(null=True)
@@ -13,3 +23,4 @@ class Post1 (models.Model):
 
     def __str__(self):
         return " {} - {} ".format(self.title,self.id)
+    
