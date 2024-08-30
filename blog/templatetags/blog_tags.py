97c6f7757bcbcef921e66759e7de7cf520a9b472
FazKal <fazelkalhory@gmail.com>
2024-08-30 18:07:09 +0330

    Created 'blog-postCategories.html'and defined inclusion tag for this file for design Post Categories in sidebar of blog-home

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from blog.models import Post1
+from blog.models import categorys
 
 register=template.Library()
 
@@ -21,3 +22,12 @@ def snippet(value,arg=20):
 def latestposts(arg=3):
     posts=Post1.objects.filter(status=1).order_by('published_date')[:arg]
     return {'posts':posts}
+
+@register.inclusion_tag('blog/blog-postCategories.html')
+def postcategories():
+    posts=Post1.objects.filter(status=1)
+    categories=categorys.objects.all()
+    cat_dict={}
+    for name in categories:
+        cat_dict[name]=posts.filter(category=name).count()
+    return {'categories': cat_dict}
