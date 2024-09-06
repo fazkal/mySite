fa99bffbafb8f56f8b71f2a786c069a7be7dc041
Fazkal <fazelkalhory@gmail.com>
2024-09-06 18:49:13 +0330

    Added path and view for search. created blog-search.html for search in sidebar

--- blog/urls.py
+++ blog/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('test',test,name='test'),
     path('category/<str:cat_name>',blog_category,name='category'),
     path('author/<str:author_username>',blog_view,name='author'),
+    path('search/',blog_search,name='search'),
     
 ]
