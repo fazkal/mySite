fd3140b9b2d7d6c4ba4ddf2cd4151f3c87d4ceb4
Fazkal <fazelkalhory@gmail.com>
2024-09-12 12:13:11 +0330

    Defined 'summernote'and another codes in mySite/settings.Added path in MySite/urls.Determined summernote_fields for 'content'.Safe content in blog-single

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 from blog.models import Post1,categorys
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     date_hierarchy='created_date'
     empty_value_display='-empty-'
     list_display=('title','author','content_view','status','published_date','created_date')
     list_filter=('status','author')
     ordering=['created_date']
     search_fields=['title','content']
+    summernote_fields=('content',)
 
 admin.site.register(Post1,PostAdmin)
 admin.site.register(categorys)
